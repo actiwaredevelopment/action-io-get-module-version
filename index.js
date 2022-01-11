803895cf33a8f655bc0bcd8298bb591a5bd1bbce
mathias.herhold <Mathias.Herhold@actiware-development.com>
2022-01-11 08:48:12 +0100

    chore: update action

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-const action = require('./action.js');
-action();
+const fs = require("fs");
+const core = require('@actions/core');
+
+const moment = require('moment');
+
+try {
+    // `module-definition-file` input defined in action metadata file
+    const moduleDefinitionFile = core.getInput('module-definition-file');
+    const fallbackVersion = core.getInput('fallback-version') ?? '2.0.0';
+
+    const time = moment().utcOffset();
+    const versionPostFix = time.format('YYMM');
+
+    if (!fs.existsSync(moduleDefinitionFile)) {
+        console.log(`  - ${moduleDefinitionFile} (Not Found)`);
+        console.log(`  - ${fallbackVersion} (Fallback)`);
+
+        core.setOutput('version', `${fallbackVersion}.${versionPostFix}`);
+    } else {
+        const moduleDefinition = JSON.parse(fs.readFileSync(moduleDefinitionFile, 'utf8'));
+
+        if (!moduleDefinition || !moduleDefinition.version) {
+            console.log(`  - ${fallbackVersion} (Fallback)`);
+
+            core.setOutput('version', `${fallbackVersion}.${versionPostFix}`);
+        } else {
+            console.log(`  - ${moduleDefinition.version}`);
+
+            core.setOutput('version', `${moduleDefinition.version}`);
+        }
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
