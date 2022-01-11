0f23b7748e23c31aaff150bdca33c5404963c1bc
mathias.herhold <Mathias.Herhold@actiware-development.com>
2022-01-11 09:15:00 +0100

    chore: update

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const fs = require("fs");
 const core = require('@actions/core');
 
 try {
+    
     // `module-definition-file` input defined in action metadata file
     const moduleDefinitionFile = core.getInput('module-definition-file');
+    const filePath = path.join(process.env.GITHUB_WORKSPACE, moduleDefinitionFile);
     const fallbackVersion = core.getInput('fallback-version') ?? '2.0.0';
 
     const now = new Date();
@@ -21,16 +23,16 @@ try {
 
     const versionPostFix = `${month}${day}`;
 
-    if (!fs.existsSync(moduleDefinitionFile)) {
-        console.log(`  - ${moduleDefinitionFile} (Not Found)`);
-        console.log(`  - ${fallbackVersion} (Fallback)`);
+    if (!fs.existsSync(filePath)) {
+        console.log(`  - ${filePath} (Not Found)`);
+        console.log(`  - ${fallbackVersion}.${versionPostFix} (Fallback)`);
 
         core.setOutput('version', `${fallbackVersion}.${versionPostFix}`);
     } else {
-        const moduleDefinition = JSON.parse(fs.readFileSync(moduleDefinitionFile, 'utf8'));
+        const moduleDefinition = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
         if (!moduleDefinition || !moduleDefinition.version) {
-            console.log(`  - ${fallbackVersion} (Fallback)`);
+            console.log(`  - ${fallbackVersion}.${versionPostFix} (Fallback)`);
 
             core.setOutput('version', `${fallbackVersion}.${versionPostFix}`);
         } else {
