d674f1faebd8b5004091f95c03fbbae84f0442fc
mathias.herhold <Mathias.Herhold@actiware-development.com>
2022-03-09 15:59:28 +0100

    feat: add option to change major and minor version

--- index.js
+++ index.js
@@ -1,47 +1,76 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const core = require('@actions/core');
 
 try {
-    
-    // `module-definition-file` input defined in action metadata file
-    const moduleDefinitionFile = core.getInput('module-definition-file');
-    const filePath = path.join(process.env.GITHUB_WORKSPACE, moduleDefinitionFile);
-    const fallbackVersion = core.getInput('fallback-version') ?? '2.0.0';
+	// `module-definition-file` input defined in action metadata file
+	const moduleDefinitionFile = core.getInput('module-definition-file');
+	const filePath = path.join(
+		process.env.GITHUB_WORKSPACE,
+		moduleDefinitionFile
+	);
+	const fallbackVersion = core.getInput('fallback-version') ?? '2.0.0';
 
-    const now = new Date();
+	const alternativeMajorVersion =
+		core.getInput('alternative-major-version') ?? '';
+	const alternativeMinorVersion =
+		core.getInput('alternative-minor-version') ?? '';
 
-    let day = now.getDay().toString();
-    let month = now.getMonth().toString();
+	const now = new Date();
 
-    if (day.length === 1) {
-        day = `0${day}`;
-    }
+	let moduleVersion = '';
 
-    if (month.length === 1) {
-        month = `0${month}`;
-    }
+	let day = now.getDay().toString();
+	let month = now.getMonth().toString();
 
-    const versionPostFix = `${month}${day}`;
+	if (day.length === 1) {
+		day = `0${day}`;
+	}
 
-    if (!fs.existsSync(filePath)) {
-        console.log(`  - ${filePath} (Not Found)`);
-        console.log(`  - ${fallbackVersion}.${versionPostFix} (Fallback)`);
+	if (month.length === 1) {
+		month = `0${month}`;
+	}
 
-        core.setOutput('version', `${fallbackVersion}.${versionPostFix}`);
-    } else {
-        const moduleDefinition = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+	const versionPostFix = `${month}${day}`;
 
-        if (!moduleDefinition || !moduleDefinition.version) {
-            console.log(`  - ${fallbackVersion}.${versionPostFix} (Fallback)`);
+	if (!fs.existsSync(filePath)) {
+		console.log(`  - ${filePath} (Not Found)`);
+		console.log(`  - ${fallbackVersion}.${versionPostFix} (Fallback)`);
 
-            core.setOutput('version', `${fallbackVersion}.${versionPostFix}`);
-        } else {
-            console.log(`  - ${moduleDefinition.version}`);
+		moduleVersion = `${fallbackVersion}.${versionPostFix}`;
+	} else {
+		const moduleDefinition = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-            core.setOutput('version', `${moduleDefinition.version}`);
-        }
-    }
+		if (!moduleDefinition || !moduleDefinition.version) {
+			console.log(`  - ${fallbackVersion}.${versionPostFix} (Fallback)`);
+
+			moduleVersion = `${fallbackVersion}.${versionPostFix}`;
+		} else {
+			console.log(`  - ${moduleDefinition.version}`);
+
+			moduleVersion = `${moduleDefinition.version}`;
+		}
+	}
+
+	if (moduleVersion) {
+		const version = moduleVersion.split('.');
+
+		if (alternativeMajorVersion && version.length > 0) {
+			version[0] = alternativeMajorVersion;
+		}
+
+		if (alternativeMinorVersion && version.length > 1) {
+			version[1] = alternativeMajorVersion;
+		}
+
+		if (version && version.length > 0) {
+			moduleVersion = version.join('.');
+		}
+	}
+
+	console.log(`Version: ${moduleVersion}`);
+
+	core.setOutput('version', `${moduleVersion}`);
 } catch (error) {
-    core.setFailed(error.message);
+	core.setFailed(error.message);
 }
